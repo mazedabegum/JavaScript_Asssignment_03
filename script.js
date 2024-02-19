5bc8182d7b821e4a6664b11fd9e8fe54a922eca4
mazedabegum <mazedabegum523@gmail.com>
2024-02-19 14:42:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const fruits = ["mango", "orrange", banana];
+
+
+
+const name = {firstName:"mazeda", lastName:"akter", };
+
+
+
+
+
+var iphonePrice = 80000;
+
+var myBudget = 50000;
+
+if (iphonePrice < myBudget){
+    console.log("Give me iphone");
+}else{
+    console.log("Give me Sumsung");
+};
+
+
+
+const cars = ["Saab", "Volvo", "BMW", "Saab",];
+
+cars.push("TATA")
+
+console.log(cars);
+
+
+
+const bike = ["yamaha", "hero", "honda", "apachi"]
+
+cars.pop("hero")
+
+console.log(cars)
+
+
+
+
